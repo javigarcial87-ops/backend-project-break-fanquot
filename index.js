4b4140b77048e0da1c1dc5be7952b503ddac9b9e
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-17 22:02:27 +0100

    pruebas backend

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/cards", async (req, res) => {
 })
 
 // 🚀 levantar servidor
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
   console.log(`Servidor en http://localhost:${PORT}`)
